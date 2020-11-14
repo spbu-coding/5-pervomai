d82608aa534dd0724643ea16252e3370370a7a21
Alisa Volik <70858770+pervomai@users.noreply.github.com>
2020-11-14 18:10:36 +0300

    checking

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void other(){
+    printf("Congratulations! You've made buffer overflow");
+    fflush(stdout);
+};
+
+void input(){
+    char buffer[8];
+    printf("Enter a word: \n");
+    scanf("%s", buffer);
+
+    printf("%p \n", &other);
+
+    printf("Your word: %s \n", buffer);
+    fflush(stdout);
+}
+
+int main() {
+    input();
+    return 0;
+}
